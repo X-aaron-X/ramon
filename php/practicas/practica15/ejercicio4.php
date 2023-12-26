7a3f231d79b7db1846fd2aa05959bfadc50b20f7
Aaron <104621430+X-aaron-X@users.noreply.github.com>
2023-12-26 19:24:05 +0100

    practica15 ejercicio4

--- php/practicas/practica15/ejercicio4.php
+++ php/practicas/practica15/ejercicio4.php
@@ -1,4 +1,8 @@
 <?php
-    // function no_space (string $s): string{
-    //     return;
-    // }
+    function no_space (string $s): string{
+        $cadena = str_replace(' ', '', $s);
+        
+        return $cadena;
+    }
+
+    echo no_space(' E ejmplo');
