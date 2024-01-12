3363047cab0f3e65b969e7ca0210d7254fcccc82
Aaron <104621430+X-aaron-X@users.noreply.github.com>
2024-01-12 18:45:17 +0100

    Update _resultado.php

--- php/ejemplos/2024-01-10/004-controles/_resultado.php
+++ php/ejemplos/2024-01-10/004-controles/_resultado.php
@@ -3,7 +3,7 @@
         <div class="col-6">
             <ul class="list-group">
                 <li class="list-group-item list-group-item-secondary">Sumar</li>
-                <li class="list-group-item"><?= $suma ?></li>
+                <li class="list-group-item list-group-item-action"><?= $suma ?></li>
             </ul>
         </div>
         
@@ -12,7 +12,7 @@
                 <li class="list-group-item list-group-item-secondary">Numeros Introducidos</li>
                 <?php
                     foreach ($numeros as $numero) {
-                        echo "<li class=\"list-group-item\">{$numero}</li>";
+                        echo "<li class=\"list-group-item list-group-item-action\">{$numero}</li>";
                     }
                 ?>
             </ul>
